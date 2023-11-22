9d93ea4532745959063abd8177ace94f28f19a09
jyj <busy.joj@gmail.com>
2023-11-22 23:50:45 +0900

    add login 페이지

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCA76bwbgJTJou5b2m5OnUXz1np_IT1Ufw',
+	authDomain: 'pokemon-98f6c.firebaseapp.com',
+	projectId: 'pokemon-98f6c',
+	storageBucket: 'pokemon-98f6c.appspot.com',
+	messagingSenderId: '326872023637',
+	appId: '1:326872023637:web:a0fc26c0c1955b02e8cadc',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
